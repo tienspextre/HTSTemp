23d7ac963990bc03c3cbbaf77932fbe2d5d65b21
Nguyen Dang Tien <98021264+tienspextre@users.noreply.github.com>
2024-04-20 17:36:22 +0700

    17h36

--- script/lam_bai.js
+++ script/lam_bai.js
@@ -1,22 +1,3 @@
-function showConfirmation() {
-    var overlay = document.getElementById('lam_bai');
-    var confirmationBox = document.getElementById('confirmationBox');
-
-    // Display overlay and confirmation box
-    overlay.style.display = 'block';
-    confirmationBox.style.display = 'block';
-}
-
-// Function to hide the overlay and confirmation box
-function hideConfirmation() {
-    var overlay = document.getElementById('lam_bai');
-    var confirmationBox = document.getElementById('confirmationBox');
-
-    // Hide overlay and confirmation box
-    overlay.style.display = 'none';
-    confirmationBox.style.display = 'none';
-}
-
 // Add click event listener to show confirmation button
 var showConfirmationButton = document.getElementById('submitBtn');
 showConfirmationButton.addEventListener('click', showConfirmation);
@@ -24,12 +5,31 @@ showConfirmationButton.addEventListener('click', showConfirmation);
 // Add click event listener to cancel button
 var cancelButton = document.getElementById('cancelButton');
 cancelButton.addEventListener('click', hideConfirmation);
-
-// Add click event listener to confirm button
 var confirmButton = document.getElementById('confirmButton');
-confirmButton.addEventListener('click', function() {
+
+function onConfirmClick() {
     // Perform your action when the confirmation button is clicked
     // For now, let's just hide the confirmation box
     hideConfirmation();
-    // alert('Bài đã được nộp!');
-});
+    window.location.href = 'xem_diem_1.html';
+};
+
+function showConfirmation() {
+    var dim = document.getElementById('dim');
+    var confirmationBox = document.getElementById('confirmationBox');
+
+    // Display overlay and confirmation box
+    dim.style.display = 'block';
+    confirmationBox.style.display = 'inline-flex';
+}
+
+// Function to hide the overlay and confirmation box
+function hideConfirmation() {
+    var dim = document.getElementById('dim');
+    var confirmationBox = document.getElementById('confirmationBox');
+
+    // Hide overlay and confirmation box
+    dim.style.display = 'none';
+    confirmationBox.style.display = 'none';
+}
+
